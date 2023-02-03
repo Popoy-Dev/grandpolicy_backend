b648684c185ff3000464cce1e259726350791537
Joyce Montaño Kua <67680548+Popoy-Dev@users.noreply.github.com>
2023-02-03 22:01:28 +0800

    504 Commit message

--- controllers/post.js
+++ controllers/post.js
@@ -74,7 +74,7 @@ exports.like = async (req, res) => {
         res.json(like)
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: error })
+        res.status(504).json({ message: error })
     }
 }
 exports.createPost = (req, res) => {
