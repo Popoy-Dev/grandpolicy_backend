feccd4a0dda3225897529235cf13e4fd0cfdaf66
Joyce Montaño Kua <67680548+Popoy-Dev@users.noreply.github.com>
2023-02-22 20:37:54 +0800

    refactor again

--- controllers/post.js
+++ controllers/post.js
@@ -39,25 +39,31 @@ exports.postById = (req, res, next, id) => {
 } */
 
 exports.getPosts = async (req, res) => {
-    try {
-      const currentPage = req.query.page || 1;
-      const perPage = 2;
-      
-      const totalItems = await Post.countDocuments();
-      
-      const posts = await Post.find()
-         .sort({ created: -1 })
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage)
-        .select("_id title body created likes photo")
+  // get current page from req.query or use default value of 1
+  const currentPage = req.query.page || 1;
+  // return 3 posts per page
+  const perPage = 2;
+  let totalItems;
 
-        
+  const posts = Post.find()
+    // countDocuments() gives you total count of posts
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+      .sort({ created: -1 })
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage)
+      .select("_id title body created likes photo")
+      .populate("comments", "text created")
+      .populate("postedBy", "_id name")
+    })
+    .then((posts) => {
       res.status(200).json(posts);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ error: "Server error" });
-    }
-  };
+    })
+    .catch((err) => console.log(err));
+    
+};
 
 exports.like = async (req, res) => {
     try {
